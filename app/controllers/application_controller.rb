823e878164f6d9192b71cc54423a5fe32ba99dfc
Nithin Gunasekaran <nithin.ktg@gmail.com>
2020-04-26 19:16:01 +0530

    Update layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def after_sign_in_path_for(resource)
-    stored_location_for(resource) || bursts_path
-  end
-
+  
   protected
 
   def configure_permitted_parameters
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@
 
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!
+  layout 'welcome'
 
   def index; end
 end
