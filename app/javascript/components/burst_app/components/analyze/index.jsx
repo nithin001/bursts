6f73b89d21d643a557741b88e6b4d2d62d6604f3
Nithin Gunasekaran <nithin.ktg@gmail.com>
2020-05-03 21:53:23 +0530

    Fix assets issue

--- app/javascript/components/burst_app/components/analyze/index.jsx
+++ app/javascript/components/burst_app/components/analyze/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loadStats } from "../../redux/actions";
 import { getStatsState } from "../../redux/selectors";
-import "./stylesheets/analyze.scss";
+import "../../stylesheets/analyze.scss";
 
 function Analyze() {
   const stats = useSelector(getStatsState);
