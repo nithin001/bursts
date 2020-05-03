c2192c50ef186932704858804e7c0b093a617afe
Nithin Gunasekaran <nithin.ktg@gmail.com>
2020-05-03 21:25:46 +0530

    Fix assets issue

--- app/javascript/components/burst_app/components/welcome/Welcome.jsx
+++ app/javascript/components/burst_app/components/welcome/Welcome.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { createBurst } from "../../redux/actions";
 import { getBurstState } from "../../redux/selectors";
-import "./welcome.scss";
+import "./stylesheets/welcome.scss";
 
 import Engineer from "images/undraw_new_ideas_jdea";
 
